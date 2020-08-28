7e83b58d9fc5d73ea6a3ff4d56a979f0b7b7931d
queyue.crk <queyue.crk@alibaba-inc.com>
2020-08-28 15:16:39 +0800

    add tensorboard support

--- pyscript/index.py
+++ pyscript/index.py
@@ -1,13 +1,16 @@
 import tensorflow as tf
 import sys
 
-def train(xs, ys, model, j, batchesPerEpoch):
+def train(xs, ys, model, j, batchesPerEpoch, trainSummaryWriter, iteration):
   xs = tf.stack(xs)
   ys = tf.stack(ys)
   trainRes = model.train_on_batch(xs, ys)
   if (j % (int(batchesPerEpoch / 10)) == 0):
     print('Iteration {}/{} result --- loss: {} accuracy: {}'.format(j, batchesPerEpoch, trainRes[0], trainRes[1]))
   sys.stdout.flush()
+  with trainSummaryWriter.as_default():
+    tf.summary.scalar('loss', trainRes[0], step=iteration)
+    tf.summary.scalar('accuracy', trainRes[1], step=iteration)
 
   
 def evaluate(xs, ys, model):
